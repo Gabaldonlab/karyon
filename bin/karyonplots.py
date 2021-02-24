1bae511d4ab04976062c58ac5fbc3120195b801d
Miguel Ángel Naranjo Ortiz <MANaranjo@users.noreply.github.com>
2021-02-24 10:56:03 -0500

    Added support for scafminsize and scafmaxsize

--- bin/karyonplots.py
+++ bin/karyonplots.py
@@ -47,7 +47,7 @@ def scaffold_len_log(fasta, window_size, fastainput, output):
 	plt.savefig(output+'_lenlog.png')
 	plt.close()
 
-def extract_vcf_data (vcf_file, quality_filter, window_size):
+def extract_vcf_data (vcf_file, quality_filter, window_size, lendict, scafminsize, scafmaxsize):
 	snp_count = 0
 	curr_pos = 0
 	curr_scaffold = ''
@@ -55,55 +55,61 @@ def extract_vcf_data (vcf_file, quality_filter, window_size):
 	for line in vcf_file:
 		if line[0] == "#": continue
 		chunk = line.split("\t")
-		if float(chunk[5]) < quality_filter: continue
-		if chunk[0] != curr_scaffold:
-			curr_scaffold = chunk[0]
-			curr_pos = 0
-			snp_dict[curr_scaffold+"_"+str(curr_pos)] = snp_count
-			snp_count = 1
-		elif int(chunk[1]) >= curr_pos + window_size:
-			snp_dict[curr_scaffold+"_"+str(curr_pos)] = snp_count
-			if (int(chunk[1])-curr_pos)/window_size > 1:
-				for i in range(1, int((int(chunk[1])-curr_pos)//window_size)):
-					snp_dict[curr_scaffold+"_"+str(curr_pos+(i*window_size))] = 0
-			curr_pos = curr_pos + (window_size*((int(chunk[1])-curr_pos)/window_size))
-			snp_count = 1
+		if scafminsize != False and lendict[chunk[0]] <= scafminsize: continue
+		elif scafmaxsize != False and lendict[chunk[0]] >= scafminsize: continue
 		else:
-			snp_count = snp_count + 1
+			if float(chunk[5]) < quality_filter: continue
+			if chunk[0] != curr_scaffold:
+				curr_scaffold = chunk[0]
+				curr_pos = 0
+				snp_dict[curr_scaffold+"_"+str(curr_pos)] = snp_count
+				snp_count = 1
+			elif int(chunk[1]) >= curr_pos + window_size:
+				snp_dict[curr_scaffold+"_"+str(curr_pos)] = snp_count
+				if (int(chunk[1])-curr_pos)/window_size > 1:
+					for i in range(1, int((int(chunk[1])-curr_pos)//window_size)):
+						snp_dict[curr_scaffold+"_"+str(curr_pos+(i*window_size))] = 0
+				curr_pos = curr_pos + (window_size*((int(chunk[1])-curr_pos)/window_size))
+				snp_count = 1
+			else:
+				snp_count = snp_count + 1
 	vcf_file.seek(0)
 	return snp_dict
 
-def extract_pileup_data (pileup_file, window_size):
+def extract_pileup_data (pileup_file, window_size, lendict, scafminsize, scafmaxsize):
 	coverage = []
 	curr_pos = 0
 	curr_scaffold = ''
 	cov_dict = {}
 	for line in pileup_file:
-		if line[0] == "#": continue
-		chunk = line.split()
-		if chunk[0] != curr_scaffold:
-			curr_scaffold = chunk[0]
-			curr_pos = 0
-			cov_dict[curr_scaffold+"_"+str(curr_pos)] = numpy.mean(coverage)
-			coverage = []
-		elif int(chunk[1]) >= curr_pos + window_size:
-			cov_dict[curr_scaffold+"_"+str(curr_pos)] = numpy.mean(coverage)
-			if (int(chunk[1])-curr_pos)/window_size > 1:
-				for i in range(1, int((int(chunk[1])-curr_pos)//window_size)):
-					cov_dict[curr_scaffold+"_"+str(curr_pos+(i*window_size))] = 0
-			curr_pos = curr_pos + (window_size*((int(chunk[1])-curr_pos)/window_size))
-			coverage.append(int(line[line.find("DP=")+3:].split(";")[0]))
+		if scafminsize != False and lendict[chunk[0]] <= scafminsize: continue
+		elif scafmaxsize != False and lendict[chunk[0]] >= scafminsize: continue
 		else:
-			coverage.append(int(line[line.find("DP=")+3:].split(";")[0]))
+			if line[0] == "#": continue
+			chunk = line.split()
+			if chunk[0] != curr_scaffold:
+				curr_scaffold = chunk[0]
+				curr_pos = 0
+				cov_dict[curr_scaffold+"_"+str(curr_pos)] = numpy.mean(coverage)
+				coverage = []
+			elif int(chunk[1]) >= curr_pos + window_size:
+				cov_dict[curr_scaffold+"_"+str(curr_pos)] = numpy.mean(coverage)
+				if (int(chunk[1])-curr_pos)/window_size > 1:
+					for i in range(1, int((int(chunk[1])-curr_pos)//window_size)):
+						cov_dict[curr_scaffold+"_"+str(curr_pos+(i*window_size))] = 0
+				curr_pos = curr_pos + (window_size*((int(chunk[1])-curr_pos)/window_size))
+				coverage.append(int(line[line.find("DP=")+3:].split(";")[0]))
+			else:
+				coverage.append(int(line[line.find("DP=")+3:].split(";")[0]))
 	pileup_file.seek(0)
 	return cov_dict
 
-def var_v_cov(vcf, pileup, window_size, output):
+def var_v_cov(vcf, pileup, window_size, output, lendict, scafminsize, scafmaxsize):
 	vcf_file = open(vcf)
 	pileup_file = open(pileup)
 	quality_filter = 500
-	snp_density = extract_vcf_data(vcf_file, quality_filter, window_size)
-	mean_cov = extract_pileup_data(pileup_file, window_size)
+	snp_density = extract_vcf_data(vcf_file, quality_filter, window_size, lendict, scafminsize, scafmaxsize)
+	mean_cov = extract_pileup_data(pileup_file, window_size, lendict, scafminsize, scafmaxsize)
 	x, y = [], []
 	fig = plt.figure(figsize=(15, 10))
 	for element in snp_density:
@@ -122,7 +128,7 @@ def var_v_cov(vcf, pileup, window_size, output):
 	plt.close()
 	return mean_cov
 
-def cov_plot(pileup, window_size, output):
+def cov_plot(pileup, window_size, output, lendict, scafminsize, scafmaxsize):
 	pileup_file = open(pileup)
 	coverage = []
 	curr_pos = 0
@@ -132,20 +138,23 @@ def cov_plot(pileup, window_size, output):
 	for line in pileup_file:
 		if line[0] == "#": continue
 		chunk = line.split()
-		if chunk[0] != curr_scaffold:
-			curr_scaffold = chunk[0]
-			curr_pos = 0
-			cov_dict[curr_scaffold+"_"+str(curr_pos)] = coverage
-			coverage = []
-		elif int(chunk[1]) >= curr_pos + window_size:
-			cov_dict[curr_scaffold+"_"+str(curr_pos)] = coverage
-			if (int(chunk[1])-curr_pos)/window_size > 1:
-				for i in range(1, int((int(chunk[1])-curr_pos)//window_size)):
-					cov_dict[curr_scaffold+"_"+str(curr_pos+(i*window_size))] = 0
-			curr_pos = curr_pos + (window_size*((int(chunk[1])-curr_pos)/window_size))
-			coverage = [int(line[line.find("DP=")+3:].split(";")[0])]
-		else: 
-			coverage.append(int(line[line.find("DP=")+3:].split(";")[0]))
+		if scafminsize != False and lendict[chunk[0]] <= scafminsize: continue
+		elif scafmaxsize != False and lendict[chunk[0]] >= scafminsize: continue
+		else:
+			if chunk[0] != curr_scaffold:
+				curr_scaffold = chunk[0]
+				curr_pos = 0
+				cov_dict[curr_scaffold+"_"+str(curr_pos)] = coverage
+				coverage = []
+			elif int(chunk[1]) >= curr_pos + window_size:
+				cov_dict[curr_scaffold+"_"+str(curr_pos)] = coverage
+				if (int(chunk[1])-curr_pos)/window_size > 1:
+					for i in range(1, int((int(chunk[1])-curr_pos)//window_size)):
+						cov_dict[curr_scaffold+"_"+str(curr_pos+(i*window_size))] = 0
+				curr_pos = curr_pos + (window_size*((int(chunk[1])-curr_pos)/window_size))
+				coverage = [int(line[line.find("DP=")+3:].split(";")[0])]
+			else: 
+				coverage.append(int(line[line.find("DP=")+3:].split(";")[0]))
 	data = []
 	meanlist = []
 	for element in cov_dict:
@@ -163,7 +172,7 @@ def cov_plot(pileup, window_size, output):
 	plt.close()
 	pileup_file.close()
 
-def fair_coin_global(vcf, window_size, output):
+def fair_coin_global(vcf, window_size, output, lendict, scafminsize, scafmaxsize):
 	vcf_file = open(vcf)
 	value_list = []
 	binomial_list = []
@@ -172,14 +181,17 @@ def fair_coin_global(vcf, window_size, output):
 		if line[0] == "#": continue
 		else:
 			chunk = line.split()[-1]
-			if chunk.split(":")[0] == "0/1":
-				values = chunk.split(':')[1].split(',')
-				if (int(values[0])+int(values[1])) == 0:
-					value_list.append(float(values[1])/1)
-					binomial_list.append(numpy.random.binomial(n=(int(values[0])+int(values[1])), p=0.5, size=None)/1)
-				else:
-					value_list.append(float(values[1])/(int(values[0])+int(values[1])))
-					binomial_list.append(numpy.random.binomial(n=(int(values[0])+int(values[1])), p=0.5, size=None)/ (float(values[0])+int(values[1])))	
+			if scafminsize != False and lendict[chunk[0]] <= scafminsize: continue
+			elif scafmaxsize != False and lendict[chunk[0]] >= scafminsize: continue
+			else:
+				if chunk.split(":")[0] == "0/1":
+					values = chunk.split(':')[1].split(',')
+					if (int(values[0])+int(values[1])) == 0:
+						value_list.append(float(values[1])/1)
+						binomial_list.append(numpy.random.binomial(n=(int(values[0])+int(values[1])), p=0.5, size=None)/1)
+					else:
+						value_list.append(float(values[1])/(int(values[0])+int(values[1])))
+						binomial_list.append(numpy.random.binomial(n=(int(values[0])+int(values[1])), p=0.5, size=None)/ (float(values[0])+int(values[1])))	
 	print (scipy.stats.chisquare(value_list, f_exp=binomial_list, ddof=0, axis=0))
 	bins = numpy.linspace(0, 1, 100)
 	sns.distplot(binomial_list, bins, label='exp')
@@ -196,7 +208,7 @@ def fair_coin_global(vcf, window_size, output):
 	vcf_file.seek(0)
 	plt.close()
 
-def fair_coin_scaff(vcf, window_size, counter, output):
+def fair_coin_scaff(vcf, window_size, counter, output, lendict, scafminsize, scafmaxsize):
 	vcf_file = open(vcf)
 	scaffold_number = counter
 	value_dict, alt_dict = {}, {}
@@ -209,18 +221,21 @@ def fair_coin_scaff(vcf, window_size, counter, output):
 		if line[0] == "#": continue
 		else:
 			chunk = line.split()
-		if chunk[0] not in value_dict:
-			if len(value_dict) >= scaffold_number: break
-			else:
-				value_dict[chunk[0]] = []
-				alt_dict[chunk[0]+"_alt"] = []
-		if chunk[-1].split(":")[0] == "0/1":
-			values = chunk[-1].split(':')[1].split(',')
-			if int(values[0])+int(values[1]) >= cutoff:
-				value_dict[chunk[0]].append(float(values[1])/(int(values[0])+int(values[1])))
-				alt_dict[chunk[0]+"_alt"].append(float(values[0])/(int(values[0])+int(values[1])))
-				binomial_value = numpy.random.binomial(n=(int(values[0])+int(values[1])), p=0.5, size=None)/ (float(values[0])+int(values[1]))
-				binomial_list.append(binomial_value)
+		if scafminsize != False and lendict[chunk[0]] <= scafminsize: continue
+		elif scafmaxsize != False and lendict[chunk[0]] >= scafminsize: continue
+		else:	
+			if chunk[0] not in value_dict:
+				if len(value_dict) >= scaffold_number: break
+				else:
+					value_dict[chunk[0]] = []
+					alt_dict[chunk[0]+"_alt"] = []
+			if chunk[-1].split(":")[0] == "0/1":
+				values = chunk[-1].split(':')[1].split(',')
+				if int(values[0])+int(values[1]) >= cutoff:
+					value_dict[chunk[0]].append(float(values[1])/(int(values[0])+int(values[1])))
+					alt_dict[chunk[0]+"_alt"].append(float(values[0])/(int(values[0])+int(values[1])))
+					binomial_value = numpy.random.binomial(n=(int(values[0])+int(values[1])), p=0.5, size=None)/ (float(values[0])+int(values[1]))
+					binomial_list.append(binomial_value)
 
 	for i in value_dict:
 		size_list.append(len(value_dict[i]))
@@ -378,81 +393,87 @@ def window_walker(window_size, step, vcf, fasta_file, bam, nQuire, kitchen, newp
 			start = start + step
 		vcf_file.seek(0)
 
-def nQuire_plot(value_list, window_size, newpath, xcov, ycov):
+def nQuire_plot(value_list, window_size, newpath, xcov, ycov, lendict, scafminsize, scafmaxsize):
 	name, x, y1, y2, y3, std_cov, mean_cov, snp_den = '', [], [], [], [], [], [], []
 	all_refalt_list, pos_list = [], []
 	for i in value_list:
 		if i[2] > 0:
 			name = i[0].split(":")[0]
-			x.append(i[1]) 
-			if i[2] > 1: y1.append(1.0)
-			else: y1.append(i[2])
-			if i[3] > 1: y2.append(1.0)
-			else: y2.append(i[3])
-			if i[4] > 1: y3.append(1.0)
-			else: y3.append(i[4])
-			std_cov.append(i[-1])
-			mean_cov.append(i[-3])
-			snp_den.append(i[-2])
-			for e in i[-1]:
+			if scafminsize != False and lendict[name] <= scafminsize: continue
+			elif scafmaxsize != False and lendict[name] >= scafminsize: continue
+			else:
+				x.append(i[1]) 
+				if i[2] > 1: y1.append(1.0)
+				else: y1.append(i[2])
+				if i[3] > 1: y2.append(1.0)
+				else: y2.append(i[3])
+				if i[4] > 1: y3.append(1.0)
+				else: y3.append(i[4])
+				std_cov.append(i[-1])
+				mean_cov.append(i[-3])
+				snp_den.append(i[-2])
+				for e in i[-1]:
 				pos_list.append(i[1])
 				all_refalt_list.append(e)
 
 	if len(x) > 0:
-		sns.set(style="darkgrid")
+		if scafminsize != False and lendict[name] <= scafminsize: continue
+		elif scafmaxsize != False and if lendict[name] >= scafminsize: continue
+		else:
+			sns.set(style="darkgrid")
 		
-		#Diploid score
-		fig, (p0,p1,p2,p3,p4) = plt.subplots(nrows=5, sharex=True, figsize=(45,15))
-		plt.title(name)
-		plt.subplot(5,1,1)
-		plt.xlim(0,x[-1])
-		plt.plot(x, y1, 'ro')
-		plt.plot(x, y1, color=(0.7,0,0), linestyle='--')
-		plt.axhline(y=0.8, color=(0,0,0), linestyle='-', linewidth=1)
-		plt.axhline(y=1, color=(0.3,0.3,0.3), linestyle='-', linewidth=1)
-		plt.ylabel('Diploid score')
+			#Diploid score
+			fig, (p0,p1,p2,p3,p4) = plt.subplots(nrows=5, sharex=True, figsize=(45,15))
+			plt.title(name)
+			plt.subplot(5,1,1)
+			plt.xlim(0,x[-1])
+			plt.plot(x, y1, 'ro')
+			plt.plot(x, y1, color=(0.7,0,0), linestyle='--')
+			plt.axhline(y=0.8, color=(0,0,0), linestyle='-', linewidth=1)
+			plt.axhline(y=1, color=(0.3,0.3,0.3), linestyle='-', linewidth=1)
+			plt.ylabel('Diploid score')
 		
-		#Triploid score
-		plt.subplot(5,1,2)
-		plt.xlim(0,x[-1])
-		plt.plot(x, y2, 'go')
-		plt.plot(x, y2, color=(0, 0.7, 0), linestyle='--')
-		plt.axhline(y=0.8, color=(0,0,0), linestyle='-', linewidth=1)
-		plt.axhline(y=1, color=(0.3, 0.3, 0.3), linestyle='-', linewidth=1)
-		plt.ylabel('Triploid score')
+			#Triploid score
+			plt.subplot(5,1,2)
+			plt.xlim(0,x[-1])
+			plt.plot(x, y2, 'go')
+			plt.plot(x, y2, color=(0, 0.7, 0), linestyle='--')
+			plt.axhline(y=0.8, color=(0,0,0), linestyle='-', linewidth=1)
+			plt.axhline(y=1, color=(0.3, 0.3, 0.3), linestyle='-', linewidth=1)
+			plt.ylabel('Triploid score')
 		
-		#Tetraploid score
-		plt.subplot(5,1,3)
-		plt.xlim(0,x[-1])
-		plt.plot(x, y3, 'bo')
-		plt.plot(x, y3, color=(0, 0, 0.7), linestyle='--')
-		plt.axhline(y=0.8, color=(0,0,0), linestyle='-', linewidth=1)
-		plt.axhline(y=1, color=(0.3, 0.3, 0.3), linestyle='-', linewidth=1)
-		plt.ylabel('Tetraploid score')
+			#Tetraploid score
+			plt.subplot(5,1,3)
+			plt.xlim(0,x[-1])
+			plt.plot(x, y3, 'bo')
+			plt.plot(x, y3, color=(0, 0, 0.7), linestyle='--')
+			plt.axhline(y=0.8, color=(0,0,0), linestyle='-', linewidth=1)
+			plt.axhline(y=1, color=(0.3, 0.3, 0.3), linestyle='-', linewidth=1)
+			plt.ylabel('Tetraploid score')
 
-		#Coverage
-		plt.subplot(5,1,4)
-		plt.xlim(0,x[-1])
-		plt.plot(xcov, ycov, color=(0.3, 0.3, 0.3), linestyle='-')
-		plt.ylabel('Depth of coverage')
+			#Coverage
+			plt.subplot(5,1,4)
+			plt.xlim(0,x[-1])
+			plt.plot(xcov, ycov, color=(0.3, 0.3, 0.3), linestyle='-')
+			plt.ylabel('Depth of coverage')
 		
-		#SNP location
-		plt.subplot(5,1,5)
-		plt.xlim(0,x[-1])
-		plt.ylim(0,200)
-		xy = np.vstack([pos_list,all_refalt_list])
-		plt.ylabel('SNP location and coverage')
-		if len(xy[0]) > 3:
-			if len(set(xy[0])) == 1:
-				xy[0][0] = xy[0][0]+10
-				xy[0][-1] = xy[0][-1]-10
-				z = gaussian_kde(xy, bw_method=0.001)(xy)
-			else:
-				z = gaussian_kde(xy)(xy)
-			plt.scatter(pos_list, all_refalt_list, c=z, s=30, edgecolor=(0.4, 0.4, 0.4))
-		plt.savefig(newpath+"nQuireplots_ws"+str(window_size)+"/"+name+".png")
-		print (newpath+"nQuireplots_ws"+str(window_size)+"/"+name+".png has been created")
-		plt.clf()	
+			#SNP location
+			plt.subplot(5,1,5)
+			plt.xlim(0,x[-1])
+			plt.ylim(0,200)
+			xy = np.vstack([pos_list,all_refalt_list])
+			plt.ylabel('SNP location and coverage')
+			if len(xy[0]) > 3:
+				if len(set(xy[0])) == 1:
+					xy[0][0] = xy[0][0]+10
+					xy[0][-1] = xy[0][-1]-10
+					z = gaussian_kde(xy, bw_method=0.001)(xy)
+				else:
+					z = gaussian_kde(xy)(xy)
+				plt.scatter(pos_list, all_refalt_list, c=z, s=30, edgecolor=(0.4, 0.4, 0.4))
+			plt.savefig(newpath+"nQuireplots_ws"+str(window_size)+"/"+name+".png")
+			print (newpath+"nQuireplots_ws"+str(window_size)+"/"+name+".png has been created")
+			plt.clf()	
 
 def katplot(fasta, library, KAT, out):
 	cmd = KAT+"kat comp -o "+out+" "+library+" "+fasta+" > "+out+".katreport"
@@ -462,7 +483,7 @@ def katplot(fasta, library, KAT, out):
 	print (cmd)
 	print ('###############')
 
-def allplots(window_size, vcf, fasta_file, bam, mpileup, library, nQuire, KAT, kitchen, newpath, counter, kitchenID, out_name):
+def allplots(window_size, vcf, fasta_file, bam, mpileup, library, nQuire, KAT, kitchen, newpath, counter, kitchenID, out_name, scafminsize, scafmaxsize):
 	if out_name==False:
 		outname = ''
 	newpath = newpath+"/"+out_name
@@ -479,13 +500,13 @@ def allplots(window_size, vcf, fasta_file, bam, mpileup, library, nQuire, KAT, k
 	step = window_size/2
 	scaffold_len_lin(fasta_file, window_size, fastainput, newpath)
 	scaffold_len_log(fasta_file, window_size, fastainput, newpath)
-	var_v_cov(vcf, mpileup, window_size, newpath)
-	cov_plot(mpileup, window_size, newpath)
-	fair_coin_global(vcf, window_size, newpath)
-	fair_coin_scaff(vcf, window_size, counter, newpath)
+	var_v_cov(vcf, mpileup, window_size, newpath, lendict, scafminsize, scafmaxsize)
+	cov_plot(mpileup, window_size, newpath, lendict, scafminsize, scafmaxsize)
+	fair_coin_global(vcf, window_size, newpath, lendict, scafminsize, scafmaxsize)
+	fair_coin_scaff(vcf, window_size, counter, newpath, lendict, scafminsize, scafmaxsize)
 	cov_v_len(mpileup, fastainput, newpath)
 	katplot(fasta_file, library, KAT, newpath)
-	window_walker(window_size, step, vcf, fasta_file, bam, nQuire, kitchen, newpath, counter)
+	window_walker(window_size, step, vcf, fasta_file, bam, nQuire, kitchen, newpath, counter, lendict, scafminsize, scafmaxsize)
 	
 
 
