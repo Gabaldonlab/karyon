fb14e6f07904227fd75215635164b6501aa30564
Miguel Ángel Naranjo Ortiz <MANaranjo@users.noreply.github.com>
2021-07-07 11:12:33 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -245,7 +245,7 @@ echo 'alias karyon="python3 $(pwd)/bin/karyon.py"' >> ~/.bashrc
 apt-get clean
 set -x; rm -rf /var/lib/apt/lists/*
 
-python3 "$SELF/../bin/create_config.py" --karyon "$SELF/../" --redundans "$SELF/dependencies/redundans/" --BWA "$dep_folder/bwa-0.7.15/" --GATK --samtools "$dep_folder/samtools-1.9/" --bcftools "$dep_folder/bcftools-1.9/" --picardtools "$dep_folder/picard-tools-$PICARD_VERSION" --SPAdes "$dep_folder/SPAdes-$SPAdes_VERSION-Linux" --nQuire "$dep_folder/nQuire/" --SOAPdenovo "$dep_folder/SOAPdenovo2-bin-LINUX-generic-r240" --trimmomatic "$dep_folder/Trimmomatic-$TRIMMOMATIC_VERSION/" --output "$SELF/../configuration.txt"
+python3 "$SELF/../bin/create_config.py" --karyon "$SELF/../" --redundans "$SELF/dependencies/redundans/" --BWA "$dep_folder/bwa-0.7.15/" --samtools "$dep_folder/samtools-1.9/" --bcftools "$dep_folder/bcftools-1.9/" --picardtools "$dep_folder/picard-tools-$PICARD_VERSION" --SPAdes "$dep_folder/SPAdes-$SPAdes_VERSION-Linux" --nQuire "$dep_folder/nQuire/" --SOAPdenovo "$dep_folder/SOAPdenovo2-bin-LINUX-generic-r240" --trimmomatic "$dep_folder/Trimmomatic-$TRIMMOMATIC_VERSION/" --output "$SELF/../configuration.txt"
 
 echo `date` "Installation finished!"
 echo "##################################################################################################"
